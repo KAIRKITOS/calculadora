4212d2331bce898a0feb676de9360e403a8a92cd
sifuentespedrozajade-ai <sifuentespedrozajade@gmail.com>
2025-12-01 18:20:48 -0600

    Create bd.php

---
+++ bd.php
@@ -0,0 +1,14 @@
+<?php
+define("SERVIDOR","localhost");
+define("USUARIO","root");
+define("PASSWD","09876543");
+define("BD","appweb");
+
+define("usuarios",array(
+    "id":,
+    "nombre":,
+    "cuanta_bancaria":,
+    "correo":,
+    "pass":
+));
+?>
